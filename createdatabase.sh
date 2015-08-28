4e0d6a868bf75d3a5e28a386dd6b58355087b031
Oskar Calvo <oscar.calvo@the-cocktail.com>
2015-08-28 10:54:56 +0200

    actualización del archivo de generación de base de datos

--- createdatabase.sh
+++ createdatabase.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+##
+#$1 == nombre del usuario que crea la base de datosva
+#$2 == nombre de la base de datos que se quiere crear
+##
 
-mysql -uroot -p -e "CREATE DATABASE $1 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci"
+mysql -u$1 -p -e "CREATE DATABASE $2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci"
